eedcb3abcbc74cda8c301ac800d9ec2ab3b2de22
luxihuang <Luxi.Huang@gmail.com>
2017-08-27 16:04:08 -0400

    added index.js, added 3 routes for Index page, new page, and 404 page.  and added nodemon

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const app = express(); //use Express
 
-//
 const port = process.env.PORT || 8080;
-
-const parser = require('body-parser');
-const urlencodedParser = parser.urlencoded({ extended: false });
-
-
-const expressValidator = require('express-validator');
 
 app.use(express.static('public')); //static assets are service under the 'public' folder
 app.set('view engine', 'ejs'); //ejs as the rendering engine
 
 const imageController = require('./controllers/images');
 
-app.get('/', imageController.get);
-app.get('/images', imageController.get);
-app.get('/images/create', imageController.new);
-app.post('/images/create', imageController.post);
+//=====before setup controller and models
+app.get('/', (request, response) => {
+    response.render('index') ;
+});
+
+
+app.get('/images/create', (request, response) => {
+    response.render('new') ;
+});
+
+
+app.get('*', (request, response) => {
+    response.render('404') ;
+});
+
+
+//=====use this after we setup controller & models
+// app.get('/', imageController.get);
+// app.get('/images', imageController.get);
+// app.get('/images/create', imageController.new);
+// app.post('/images/create', imageController.post);
 
 
 const server = app.listen(port, () => {
