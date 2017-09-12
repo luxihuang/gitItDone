027056daed5aeb4ff5bfdfb74b030b28b48e82c1
xanaday <kate.kolbell@gmail.com>
2017-09-11 21:13:43 -0400

    updated setup

--- models/setup.js
+++ models/setup.js
@@ -1,5 +1,7 @@
 const Images = require('./images');
 const Users = require('./users');
+const Comments = require('./commentsTable');
+const Tags = require('./tagsTable');
 
 Images.sync({force: true});
 Users.sync();
